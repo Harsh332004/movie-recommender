501076b0aef0801fa20faca227ce154e9bac249d
Harsh332004 <158804356+Harsh332004@users.noreply.github.com>
2025-07-21 17:07:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+import numpy as np
+
+st.title('Movie Recommendation System')
+
+# Load pickled files
+dataset = pickle.load(open('movies.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Get movie titles
+movies_list = dataset['title_x'].values
+option = st.selectbox('Select a movie:', movies_list)
+
+# Recommend function
+def recommend(movie_name):
+    index = dataset[dataset['title_x'] == movie_name].index[0]
+    movie_list = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    for i in movie_list:
+        recommended_movies.append(dataset.iloc[i[0]].title_x)
+    return recommended_movies
+
+# Button
+if st.button("Recommend"):
+    recommendations = recommend(option)
+    for i in recommendations:
+        st.write(i)
+else:
+    st.write("Select a movie and press recommend.")
+    
